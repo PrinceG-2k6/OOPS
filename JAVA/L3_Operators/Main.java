e30ebbcb073e1710e3990ab63b31bbd3f880e329
PrinceG-2k6 <princegupta14311@gmail.com>
2025-10-29 19:42:43 +0530

    .

---
+++ JAVA/L3_Operators/Main.java
@@ -0,0 +1,23 @@
+package L3_Operators;
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 2;
+        int z;
+        //Arithmetic
+        // z = x + y;
+        // z= x- y;
+        // z= x*y;
+        // z=x/y;
+        z=x%y;
+
+// Augmented
+        // x+=y;
+
+        //Increment and Decrement
+        x++;
+        x--;
+        System.out.println(z);
+    }
+}

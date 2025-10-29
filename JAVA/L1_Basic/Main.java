adc0c62bebdc4de3d48b7e85b8927ab87da50d2f
PrinceG-2k6 <princegupta14311@gmail.com>
2025-10-29 19:02:34 +0530

    .

---
+++ JAVA/L1_Basic/Main.java
@@ -0,0 +1,27 @@
+package L1_Basic;
+
+public class Main {
+    public static void main(String[] args) {
+        // store roll no
+        int[] numbers = new int[5];
+        numbers[0] = 0;
+        numbers[1] = 1;
+        numbers[2] = 2;
+        numbers[3] = 3;
+        numbers[4] = 4;
+
+        // store 5 names
+        String[] names = new String[5];
+        names[0] = "hello";
+        names[1] = "hello";
+        names[2] = "hello";
+        names[3] = "hello";
+        names[4] = "hello";
+        for (int i = 0; i < 5; i++) {
+            System.out.println(numbers[i]);
+        }
+
+        // store 5 students data
+    }
+
+}

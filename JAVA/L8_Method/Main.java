2fd7d6815d1c10dba816ffda086af6482d02b0ec
PrinceG-2k6 <princegupta14311@gmail.com>
2025-10-30 20:03:28 +0530

    .

---
+++ JAVA/L8_Method/Main.java
@@ -0,0 +1,22 @@
+package L8_Method;
+
+public class Main {
+    public static void main(String[] args) {
+        String name ="Prince";
+        int age =19;
+        HappyBirthDay(name,age);
+    }
+
+    static void HappyBirthDay(String name,int age) {
+        System.out.println("Happy Birthday To You");
+        System.out.println("Happy Birthday To You");
+        System.out.println("Happy Birthday Dear "+name);
+        System.out.println("You are "+age+" years old");
+        System.out.println("May God Bless You");
+        System.out.println("Happy Birthday To You");
+        System.out.println("-------------------------------------------");
+
+
+    }
+
+}
